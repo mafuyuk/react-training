7c1a0395c8c44d47da2d4f04759c632bb31f5bde
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-06-14 15:55:49 +0900

    Updated props

--- react-textbook-on-the-development-site/src/index.js
+++ react-textbook-on-the-development-site/src/index.js
@@ -2,8 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Hello from "./hello";
 import Hello2 from "./hello2";
+import Profile from "./profile";
 
 import "./styles.css";
+
+const taro = {
+  name: 'Taro',
+  age: 20,
+  love: 'Game',
+};
+
+const hanako = {
+    name: 'Hanako',
+    age: 10,
+    love: 'Cats',
+};
+
 
 function Index() {
     return (
@@ -12,6 +26,8 @@ function Index() {
             <h2>Start editing to see some magic happen!</h2>
             <Hello />
             <Hello2 />
+            <Profile data={taro}/>
+            <Profile data={hanako}/>
         </div>
     );
 }
