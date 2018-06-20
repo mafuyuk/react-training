65f031ca44f3c54837ed083dc17e1164f0dae62a
kamono-mafuyu <kamono-mafuyu@dmm.com>
2018-06-20 17:57:17 +0900

    Added develop tool

--- react-textbook-on-the-development-site-redux/src/store/index.js
+++ react-textbook-on-the-development-site-redux/src/store/index.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import todoApp from '../reducers/index';
 import logger from '../middleware/logger';
 
-let store = createStore(
-  todoApp,
+let store = createStore(todoApp, composeWithDevTools(
   applyMiddleware(logger)
-);
+));
 
 export default store;
